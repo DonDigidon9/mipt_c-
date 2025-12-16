85b810287e388bbd03a541b5d5239ae776498223
DonDigidon <146740604+DonDigidon9@users.noreply.github.com>
2025-12-16 16:32:48 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,25 +3,36 @@
 #include <cmath>
 #include <regex>
 #include <cassert>
+#include <unordered_map>
 
 bool isDouble(const std::string& str) {
-    // Регулярное выражение для double
-    std::regex double_regex(R"(^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$)");
-    return std::regex_match(str, double_regex);
+  // Регулярное выражение для double
+  std::regex double_regex(R"(^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$)");
+  return std::regex_match(str, double_regex);
 }
 
 class Operation {
+ protected:
+  int args_count;
+  
  public:
+  Operation(int count) : args_count(count) {}
+  
   virtual double calculate(std::stack<std::string>& st) const = 0;
   virtual ~Operation() = default;
+  
+  int getArgsCount() const {
+    return args_count;
+  }
 };
 
 class Sin : public Operation {
  public:
+  Sin() : Operation(1) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 1);
-    double a;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
     return std::sin(a);
   }
@@ -29,10 +40,11 @@ class Sin : public Operation {
 
 class Cos : public Operation {
  public:
+  Cos() : Operation(1) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 1);
-    double a;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
     return std::cos(a);
   }
@@ -40,10 +52,11 @@ class Cos : public Operation {
 
 class Tg : public Operation {
  public:
+  Tg() : Operation(1) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 1);
-    double a;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
     return std::tan(a);
   }
@@ -51,10 +64,11 @@ class Tg : public Operation {
 
 class Ctg : public Operation {
  public:
+  Ctg() : Operation(1) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 1);
-    double a;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
     return 1.0 / std::tan(a);
   }
@@ -62,10 +76,11 @@ class Ctg : public Operation {
 
 class Exp : public Operation {
  public:
+  Exp() : Operation(1) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 1);
-    double a;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
     return std::exp(a);
   }
@@ -73,10 +88,11 @@ class Exp : public Operation {
 
 class Log : public Operation {
  public:
+  Log() : Operation(1) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 1);
-    double a;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
     return std::log(a);
   }
@@ -84,10 +100,11 @@ class Log : public Operation {
 
 class Sqrt : public Operation {
  public:
+  Sqrt() : Operation(1) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 1);
-    double a;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
     return std::sqrt(a);
   }
@@ -95,12 +112,13 @@ class Sqrt : public Operation {
 
 class Sum : public Operation {
  public:
+  Sum() : Operation(2) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 2);
-    double a, b;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
-    b = std::stod(st.top());
+    double b = std::stod(st.top());
     st.pop();
     return a + b;
   }
@@ -108,12 +126,13 @@ class Sum : public Operation {
 
 class Diff : public Operation {
  public:
+  Diff() : Operation(2) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 2);
-    double a, b;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
-    b = std::stod(st.top());
+    double b = std::stod(st.top());
     st.pop();
     return b - a;
   }
@@ -121,12 +140,13 @@ class Diff : public Operation {
 
 class Prod : public Operation {
  public:
+  Prod() : Operation(2) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 2);
-    double a, b;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
-    b = std::stod(st.top());
+    double b = std::stod(st.top());
     st.pop();
     return a * b;
   }
@@ -134,12 +154,13 @@ class Prod : public Operation {
 
 class Div : public Operation {
  public:
+  Div() : Operation(2) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 2);
-    double a, b;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
-    b = std::stod(st.top());
+    double b = std::stod(st.top());
     st.pop();
     return b / a;
   }
@@ -147,12 +168,13 @@ class Div : public Operation {
 
 class Atan2 : public Operation {
  public:
+  Atan2() : Operation(2) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 2);
-    double a, b;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
-    b = std::stod(st.top());
+    double b = std::stod(st.top());
     st.pop();
     return std::atan2(b, a);
   }
@@ -160,94 +182,143 @@ class Atan2 : public Operation {
 
 class Pow : public Operation {
  public:
+  Pow() : Operation(2) {}
+  
   double calculate(std::stack<std::string>& st) const override final {
-    assert(std::size(st) >= 2);
-    double a, b;
-    a = std::stod(st.top());
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
     st.pop();
-    b = std::stod(st.top());
+    double b = std::stod(st.top());
     st.pop();
     return std::pow(b, a);
   }
 };
 
-int main() {
-  Sin sin;
-  Cos cos;
-  Tg tg;
-  Ctg ctg;
-  Exp exp;
-  Log log;
-  Sqrt sqrt;
-  Sum sum;
-  Diff diff;
-  Prod prod;
-  Div div;
-  Atan2 atan2;
-  Pow pow;
+class Median : public Operation {
+ public:
+  Median() : Operation(3) {}
+  
+  double calculate(std::stack<std::string>& st) const override final {
+    assert(std::size(st) >= args_count);
+    double a = std::stod(st.top());
+    st.pop();
+    double b = std::stod(st.top());
+    st.pop();
+    double c = std::stod(st.top());
+    st.pop();
+    if ((a <= b && b <= c) || (c <= b && b <= a)) return b;
+    if ((b <= a && a <= c) || (c <= a && a <= b)) return a;
+    return c;
+  }
+};
 
+class Calculator {
+ private:
+  std::unordered_map<std::string, std::unique_ptr<Operation>> operations_;
   std::stack<std::string> st;
-  std::string x;
-  while (std::cin >> x) {
-    if (x == "sin") {
-      double t = sin.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "cos") {
-      double t = cos.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "tg") {
-      double t = tg.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "ctg") {
-      double t = ctg.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "exp") {
-      double t = exp.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "log") {
-      double t = log.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "sqrt") {
-      double t = sqrt.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "+") {
-      double t = sum.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "-") {
-      double t = diff.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "*") {
-      double t = prod.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "/") {
-      double t = div.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "atan2") {
-      double t = atan2.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else if (x == "pow") {
-      double t = pow.calculate(st);
-      std::cout << t << '\n';
-      st.push(std::to_string(t));
-    } else {
-      assert(isDouble(x) and x.size() > 0);
-      st.push(x);
-    }
+  
+ public:
+  Calculator() {
+    operations_["sin"] = std::unique_ptr<Operation>(new Sin());
+    operations_["cos"] = std::unique_ptr<Operation>(new Cos());
+    operations_["tg"] = std::unique_ptr<Operation>(new Tg());
+    operations_["ctg"] = std::unique_ptr<Operation>(new Ctg());
+    operations_["exp"] = std::unique_ptr<Operation>(new Exp());
+    operations_["log"] = std::unique_ptr<Operation>(new Log());
+    operations_["sqrt"] = std::unique_ptr<Operation>(new Sqrt());
+    operations_["+"] = std::unique_ptr<Operation>(new Sum());
+    operations_["-"] = std::unique_ptr<Operation>(new Diff());
+    operations_["*"] = std::unique_ptr<Operation>(new Prod());
+    operations_["/"] = std::unique_ptr<Operation>(new Div());
+    operations_["atan2"] = std::unique_ptr<Operation>(new Atan2());
+    operations_["pow"] = std::unique_ptr<Operation>(new Pow());
+    operations_["median"] = std::unique_ptr<Operation>(new Median());
   }
   
-  std::cout << sum.calculate(st) << '\n';
+  double calculate(const std::string& expression) {
+    size_t start = 0;
+    size_t end = 0;
+    
+    while (end < expression.size()) {
+      while (start < expression.size() && expression[start] == ' ') {
+        start++;
+      }
+      
+      if (start >= expression.size()) break;
+      
+      end = start;
+      while (end < expression.size() && expression[end] != ' ') {
+        end++;
+      }
+      
+      std::string token = expression.substr(start, end - start);
+      auto it = operations_.find(token);
+      if (it != operations_.end()) {
+        assert(std::size(st) >= it->second->getArgsCount());
+        double result = it->second->calculate(st);
+        st.push(std::to_string(result));
+      } else {
+        assert(isDouble(token));
+        st.push(token);
+      }
+      
+      start = end;
+    }
+    
+    assert(std::size(st) == 1);
+    double result = std::stod(st.top());
+    
+    return result;
+  }
+};
 
+bool equal(double x, double y) {
+  return fabs(x - y) < 0.0001;
+}
+
+void test1() {
+  Calculator calc;
+  double result = calc.calculate("5 3 +");
+  assert(equal(result, 8.0));
+}
+
+void test2() {
+  Calculator calc;
+  double result = calc.calculate("10 4 -");
+  assert(equal(result, 6.0));
+}
+
+void test3() {
+  Calculator calc;
+  double result = calc.calculate("3 7 *");
+  assert(equal(result, 21.0));
+}
+
+void test4() {
+  Calculator calc;
+  double result = calc.calculate("15 3 /");
+  assert(equal(result, 5.0));
+}
+
+void test5() {
+  Calculator calc;
+  double result = calc.calculate("2 3 4 + *");
+  assert(equal(result, 14.0));
+}
+
+int main() {
+  // Calculator calculator;
+  // std::string expression;
+  // std::cout << "Введите выражение: ";
+  // std::getline(std::cin, expression);
+  // double result = calculator.calculate(expression);
+  // std::cout << "Результат: " << result << '\n';
+
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  
   return 0;
 }
